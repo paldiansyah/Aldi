614ee30757a1649297f97db3e6e4f9e2f08b3245
paldiansyah <aldipaldiansyah4@gmail.com>
2024-10-29 22:00:01 +0800

    Add files via upload

---
+++ tipe data boolean .py
@@ -0,0 +1,10 @@
+data_boolean = "true atau false"
+print (data_boolean)
+# Mendefinisikan variabel boolean
+is_raining = True
+is_weekend = False
+
+# Menggunakan operasi logika
+go_outside = is_raining == False and is_weekend == True
+
+print("Bisa keluar rumah:", go_outside)
