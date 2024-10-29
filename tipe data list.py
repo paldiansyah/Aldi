6685e2cd5c35d7b9cfe734a7265fd4d6f36822e1
paldiansyah <aldipaldiansyah4@gmail.com>
2024-10-29 22:01:20 +0800

    Add files via upload

---
+++ tipe data list.py
@@ -0,0 +1,8 @@
+list = "aldi,yusril,ritman,padli"
+print (list
+# Mendefinisikan list
+fruits = ["apple", "banana", "cherry"]
+
+# Mengakses elemen dalam list
+print("Buah pertama:", fruits[0])  # Output: apple
+print("Buah kedua:", fruits[1])     # Output: banana)
