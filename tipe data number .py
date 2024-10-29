d0c1d2c3ab7ff3205e67f27a48054edfababc814
paldiansyah <aldipaldiansyah4@gmail.com>
2024-10-29 22:02:10 +0800

    Add files via upload

---
+++ tipe data number .py
@@ -0,0 +1,14 @@
+# Mendefinisikan angka
+a = 10
+b = 5
+
+# Operasi dasar
+penjumlahan = a + b
+pengurangan = a - b
+perkalian = a * b
+pembagian = a / b
+
+print("Penjumlahan:", penjumlahan)
+print("Pengurangan:", pengurangan)
+print("Perkalian:", perkalian)
+print("Pembagian:", pembagian)
