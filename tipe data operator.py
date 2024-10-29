aa7fe1bf95807d431aba874023404b13f6fcaef0
paldiansyah <aldipaldiansyah4@gmail.com>
2024-10-29 22:02:52 +0800

    Add files via upload

---
+++ tipe data operator.py
@@ -0,0 +1,18 @@
+operator = "operator aretmatika"
+print (operator)
+# Mendefinisikan dua angka
+a = 10
+b = 5
+
+# Menggunakan operator aritmatika
+penjumlahan = a + b
+pengurangan = a - b
+perkalian = a * b
+pembagian = a / b
+modulus = a % b
+
+print("Penjumlahan:", penjumlahan)
+print("Pengurangan:", pengurangan)
+print("Perkalian:", perkalian)
+print("Pembagian:", pembagian)
+print("Modulus:", modulus)
