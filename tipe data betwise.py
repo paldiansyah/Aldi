3c371c61eda2967f869ccf609f0a7d5f265efdf8
paldiansyah <aldipaldiansyah4@gmail.com>
2024-10-29 21:58:27 +0800

    Add files via upload

---
+++ tipe data betwise.py
@@ -0,0 +1,25 @@
+Bitwise = "Bitwise adalah operator khusus untuk menangani operasi \nlogika bilangan biner dalam bentuk bit."
+print (Bitwise)
+
+x     = 1010 
+y     = 1100 
+print (x & y)
+
+x     = 1010 
+y     = 1100 
+print (x | y)
+
+x = 60
+y = 40
+ 
+print('x berisi angka',x ,'desimal atau',bin(x),'biner')
+print('y berisi angka',y ,'desimal atau',bin(y),'biner')
+ 
+print('\n')
+ 
+print('x & y  :',x & y)
+print('x | y  :',x | y)
+print('x ^ y  :',x ^ y)
+print('~x     :',~x)
+print('x << 1 :',x << 1)
+print('x >> 1 :',x >> 1)
