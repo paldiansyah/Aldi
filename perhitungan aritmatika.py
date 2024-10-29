7d1e796a702abdf45c0fe5f071abaa978b5be3ce
paldiansyah <aldipaldiansyah4@gmail.com>
2024-10-29 21:51:12 +0800

    Add files via upload

---
+++ perhitungan aritmatika.py
@@ -0,0 +1,20 @@
+#penambahan
+a = 20
+b = 30
+hasil = a + b
+print(hasil)
+#pengurangan
+c = 30.40
+d = 20.30
+hasil = c - d
+print(hasil)
+#perkalian
+e = 400000
+f = 500000
+hasil = e * f
+print(hasil)
+#pembagian
+g = 25.2
+h = 25.5
+hasil = g / h
+print(hasil)
