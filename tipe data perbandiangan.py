38007a28c9ba8d1636ff4f2c0bf95b4023f53547
paldiansyah <aldipaldiansyah4@gmail.com>
2024-10-29 22:04:35 +0800

    Add files via upload

---
+++ tipe data perbandiangan.py
@@ -0,0 +1,13 @@
+perbandingan = "saya suka perbandingan,saya sukabelajar kalkulus"
+print (perbandingan)
+# Mendefinisikan dua variabel
+a = 10
+b = 20
+
+# Menggunakan operator perbandingan
+print("a sama dengan b:", a == b)        # False
+print("a tidak sama dengan b:", a != b)  # True
+print("a lebih besar dari b:", a > b)    # False
+print("a lebih kecil dari b:", a < b)    # True
+print("a lebih besar atau sama dengan b:", a >= b)  # False
+print("a lebih kecil atau sama dengan b:", a <= b)  # True
